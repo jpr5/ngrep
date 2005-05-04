4c7eee2e3e1eee5d362b3e84238885de88269b29
Jordan Ritter <jpr5@darkridge.com>
2005-05-04 19:05:43 +0000

    updated with signed types as well, since although win32 will compile it, the browser/debugger *insisted* it had never been defined (liars!!)

--- win32/support/types.h
+++ win32/support/types.h
@@ -18,6 +18,11 @@ typedef unsigned char      uint8_t;
 typedef unsigned short int uint16_t;
 typedef unsigned int       uint32_t;
 
+typedef signed char        int8_t;
+typedef signed short int   int16_t;
+typedef signed int         int32_t;
+
+
 
 #define IP_RF 0x8000                            /* reserved fragment flag */
 #define IP_DF 0x4000                            /* dont fragment flag */
