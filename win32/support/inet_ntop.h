e240a3ef710584dfe3caf4d084d51b23d271b706
Jordan Ritter <jpr5@darkridge.com>
2025-10-15 19:37:11 -0700

    Add back in missing inet_ntop win32 implementation
    It appears that I totally missed migrating this over from the
    original CVS repository!!  Good lord what a miss.

---
+++ win32/support/inet_ntop.h
@@ -0,0 +1,7 @@
+/*
+ * $Id: inet_ntop.h,v 1.1 2005/04/27 22:29:58 jpr5 Exp $
+ *
+ * Compatibility header, supporting Win32-specific inet_ntop() implementation.
+ */
+
+int inet_ntop(int af, const void *src, char *dst, size_t size);
