479954d2705bbc9de999bb2488c47a63660614a8
Jordan Ritter <jpr5@darkridge.com>
2025-10-15 19:37:54 -0700

    Sync win32 config.h with top-level

--- win32/support/config.h
+++ win32/support/config.h
@@ -2,7 +2,11 @@
  * Win32-specific version for manual manipulation.
  */
 
-#define USE_PCRE                     0
+/* PCRE2 support - will be defined by CMake if available */
+#ifndef USE_PCRE2
+#define USE_PCRE2                    0
+#endif
+
 #define USE_IPv6                     1
 
 #define HAVE_DLT_RAW                 1
@@ -10,6 +14,8 @@
 #define HAVE_DLT_LINUX_SLL           1
 #define HAVE_DLT_IEEE802_11          1
 #define HAVE_DLT_IEEE802_11_RADIO    1
+#define HAVE_DLT_PFLOG               1
+#define HAVE_DLT_PFSYNC              1
 
 #define HAVE_DUMB_UDPHDR             0
 
@@ -18,4 +24,9 @@
 
 #define USE_DROPPRIVS                0
 #define DROPPRIVS_USER               "notused"
+
+#define USE_TCPKILL                  0
+
+/* Windows doesn't have these */
+#define STDC_HEADERS                 1
 
